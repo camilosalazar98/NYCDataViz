419a562963762c6132d3cd9fdc58d4c409159d86
Abdullah Khan <ajkhan4096@gmail.com>
2017-09-27 21:22:06 -0400

    WIP

--- app/views.py
+++ app/views.py
@@ -2,6 +2,11 @@ from app import app
 
 from flask import render_template
 from flask import Response
+from flask import jsonify
+
+from sodapy import Socrata
+
+client = Socrata('data.cityofnewyork.us', None)
 
 @app.route('/')
 @app.route('/index.html')
@@ -10,3 +15,11 @@ def index():
     Returns a static webpage for now.
     '''
     return render_template('index.html')
+
+
+@app.route('/demo/')
+def request_demo():
+    global client
+    two_points = client.get('fhrw-4uyv', limit=2)
+    response = Response(response=jsonify(two_points), status=200)
+    return response
